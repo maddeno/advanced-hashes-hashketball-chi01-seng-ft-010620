23f1019230f94c09216e0ebeca35cf66850c72f1
Olivia Madden <madden.livi@gmail.com>
2020-01-04 06:21:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,7 +127,10 @@ end
 
 def num_points_scored(player_name)
   players_array = game_hash[:home][:players] + game_hash[:away][:players]
-  players_array.each do |points
+  players_array.each do |points|
+    if players_array[:player_name] == player_name
+      points = players_array[:points]
+    end
   end
   points
 end
